f2e43c5384c843a2817d246416ae7d40048aaa1d
huxiaoou <516984451@qq.com>
2026-06-03 23:01:38 +0800

    Add battle and unit

---
+++ scenes/unit.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+class_name Unit
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var origin_y_scale: float = 1.0
+
+func _ready() -> void:
+    origin_y_scale = scale.y
+
+
+func _process(delta: float) -> void:
+    scale.y = origin_y_scale *(1 + 0.005 * sin(0.2*Time.get_ticks_msec()*delta))
