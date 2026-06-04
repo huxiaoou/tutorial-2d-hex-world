7e61612625c7534375ef51673fe03ce180cb3a21
huxiaoou <516984451@qq.com>
2026-06-04 21:14:26 +0800

    Test more backgrounds

--- scenes/unit.gd
+++ scenes/unit.gd
@@ -6,9 +6,10 @@ class_name Unit
 
 var origin_y_scale: float = 1.0
 
+
 func _ready() -> void:
     origin_y_scale = scale.y
 
 
 func _process(delta: float) -> void:
-    scale.y = origin_y_scale *(1 + 0.005 * sin(0.2*Time.get_ticks_msec()*delta))
+    scale.y = origin_y_scale * (1 + 0.005 * sin(0.2 * Time.get_ticks_msec() * delta))
