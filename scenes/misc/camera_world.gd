9bb26158fd02e2c8f33175923465eb9eb8427252
huxiaoou <516984451@qq.com>
2026-06-13 16:15:58 +0800

    Test dungeon with watabou

--- scenes/misc/camera_world.gd
+++ scenes/misc/camera_world.gd
@@ -3,11 +3,23 @@ extends Camera2D
 class_name CameraWorld
 
 @export_group("Custom")
-@export var move_speed: float = 1000
-@export var init_pos: Vector2 = Vector2(1920, 1080) *0.5
+@export var move_speed: float = 1200
+@export var init_pos: Vector2 = Vector2(0, 0)
+
+var lim_left_top: Vector2 = Vector2.ZERO
+var lim_right_bottom: Vector2 = Vector2.ZERO
+
 
 func _ready() -> void:
     position = init_pos
+    print("CameraWorld ready")
+
+
+func set_limits(left_top: Vector2, right_bottom: Vector2) -> void:
+    lim_left_top = left_top + get_viewport().size * 0.5
+    lim_right_bottom = right_bottom - get_viewport().size * 0.5
+    return
+
 
 func _process(delta: float) -> void:
     var direction: Vector2 = Input.get_vector(
@@ -17,4 +29,6 @@ func _process(delta: float) -> void:
         "camera_move_down",
     )
     position += direction * move_speed * delta
+    position.x = clamp(position.x, lim_left_top.x, lim_right_bottom.x)
+    position.y = clamp(position.y, lim_left_top.y, lim_right_bottom.y)
     return
---
+++ scenes/world/level_dungeon.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+class_name LevelDungeon
+
+@onready var camera_world: CameraWorld = $CameraWorld
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+func _ready() -> void:
+    var map_size: Vector2i = sprite_2d.texture.get_size()
+    camera_world.set_limits(-map_size * 0.5, map_size * 0.5)
+    print("LevelDungeon ready")
