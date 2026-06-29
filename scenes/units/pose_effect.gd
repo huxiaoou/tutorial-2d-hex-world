d46fc3c5d87fefc0c3f8a238e1963e93d6a69417
huxiaoou <516984451@qq.com>
2026-06-29 21:28:18 +0800

    Add background to pose_effect

--- scenes/units/pose_effect.gd
+++ scenes/units/pose_effect.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 class_name PoseEffect
 
+@onready var background: TextureRect = $Background
 @onready var ghost_trail: GhostTrail = $TextureRect
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -32,3 +33,15 @@ func play_lines() -> void:
 func play_weapon_impact() -> void:
     audio_stream_player_2d.stream = WEAPON_IMPACT
     audio_stream_player_2d.play()
+
+
+func background_fade_in() -> void:
+    var tw: Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+    tw.tween_property(background, "modulate:a", 0.8, 1.0)
+    return
+
+
+func background_fade_out() -> void:
+    var tw: Tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+    tw.tween_property(background, "modulate:a", 0.0, 1.0)
+    return
