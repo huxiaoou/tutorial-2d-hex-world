10904551599b735605c1bc7d679ba7e0fc357912
huxiaoou <516984451@qq.com>
2026-07-01 22:59:04 +0800

    Refactor animation of Unit8D

--- scenes/units/unit_8d.gd
+++ scenes/units/unit_8d.gd
@@ -2,17 +2,14 @@ extends CharacterBody2D
 
 class_name Unit8D
 
-@onready var body: AnimatedSprite2D = $body
-@onready var shadow: AnimatedSprite2D = $shadow
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("toggle_debug"):
         play_attack()
 
 func play_attack() -> void:        
-    body.play("attack")
-    shadow.play("attack")
-    await body.animation_finished
-    body.play("idle")
-    shadow.play("idle")
+    animation_player.play("attack")
+    await animation_player.animation_finished
+    animation_player.play("idle")
     return
